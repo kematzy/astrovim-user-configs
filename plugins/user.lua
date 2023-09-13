7be754a4a84b7850bac6e24118a6316a6c4f513f
Kematzy <kematzy@gmail.com>
2023-09-13 18:04:19 +0800

    Add TODO comments plugin

--- plugins/user.lua
+++ plugins/user.lua
@@ -9,4 +9,16 @@ return {
   --     require("lsp_signature").setup()
   --   end,
   -- },
+  {
+    "folke/todo-comments.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    opts = {
+      -- custom configurations should be added here
+    },
+    event = "User AstroFile",
+    cmd = { "TodoQuickFix" },
+    keys = {
+      { "<leader>T", "<cmd>TodoTelescope<cr>", desc = "Open TODOs in Telescope" },
+    },
+  },
 }
