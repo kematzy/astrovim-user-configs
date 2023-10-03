33b8bba97551281d49563b248824526eef48206f
Kematzy <kematzy@gmail.com>
2023-10-03 08:37:14 +0800

    Add Rubocop, PhpStan & phpCS linters

--- plugins/mason.lua
+++ plugins/mason.lua
@@ -41,8 +41,13 @@ return {
     opts = function(_, opts)
       -- add more things to the ensure_installed table protecting against community packs modifying it
       opts.ensure_installed = require("astronvim.utils").list_insert_unique(opts.ensure_installed, {
+        -- FORMATTERS
         "prettier",
         "stylua",
+        -- LINTERS
+        "rubocop",
+        "phpstan",
+        "phpcs",
       })
     end,
   },
